a4fb7abbae0129ea54dc322369a8d3539aa82eeb
maitreivre <98763514+lemaitreivre@users.noreply.github.com>
2024-05-01 16:36:06 +0500

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,10 +1,2 @@
-import os
-
-# Получить список файлов и директорий в каталоге /root
-files = os.listdir("/root")
-
-# Перенаправить вывод в файл /tmp/123.txt
-with open("/tmp/123.txt", "w") as f:
-    # Вывести содержимое каталога /tmp в файл
-    for file in files:
-        f.write(file + "\n")
+with open("/root/flag", "r") as f:
+    flag = f.read()
